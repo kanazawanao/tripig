a87d2aa17e8989669841f74e66ec6e8c3f72b3d2
kanazawanao <naomiti3333@gmail.com>
2019-11-12 13:43:03 +0900

    display google-map

--- src/app/pages/map-pages/map-point-search/map-point-search.component.ts
+++ src/app/pages/map-pages/map-point-search/map-point-search.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MapInfoWindow, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-map-point-search',
@@ -6,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map-point-search.component.scss'],
 })
 export class MapPointSearchComponent implements OnInit {
+  @ViewChild(MapInfoWindow, {static: false}) infoWindow!: MapInfoWindow;
+  markerOptions = {draggable: false};
+  markerPositions: google.maps.LatLngLiteral[] = [];
+  zoom = 16;
+  display?: google.maps.LatLngLiteral;
 
   constructor() { }
 
   ngOnInit() {}
 
+  addMarker(event: google.maps.MouseEvent) {
+    this.markerPositions.push(event.latLng.toJSON());
+  }
+
+  move(event: google.maps.MouseEvent) {
+    this.display = event.latLng.toJSON();
+  }
+
+  openInfoWindow(marker: MapMarker) {
+    this.infoWindow.open(marker);
+  }
+
+  removeLastMarker() {
+    this.markerPositions.pop();
+  }
 }
--- src/app/pages/map-pages/map-point-search/map-point-search.module.ts
+++ src/app/pages/map-pages/map-point-search/map-point-search.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { GoogleMapsModule } from '@angular/google-maps';
 import { MapPointSearchComponent } from './map-point-search.component';
 
-
-
 @NgModule({
-  declarations: [],
+  declarations: [MapPointSearchComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: '', component: MapPointSearchComponent }]),
+    GoogleMapsModule,
   ]
 })
 export class MapPointSearchModule { }
