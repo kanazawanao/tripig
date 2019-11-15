c41d44a738c287612a9b17036f7149a045f14c7f
kanazawanao <naomiti3333@gmail.com>
2019-11-15 18:06:51 +0900

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Platform, ModalController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Store } from '@ngrx/store';
+import * as TripigState from 'src/app/store/';
+import * as TripigActions from 'src/app/store/tripig.action';
+import * as TripigSelector from 'src/app/store/tripig.selector';
 import { MapRouteResultComponent } from './parts/dialog/map/map-route-result/map-route-result.component';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  showResultRoute = false;
+  selectedList$: Observable<google.maps.places.PlaceResult[]> = this.store.select(
+    TripigSelector.getSelectedList
+  );
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
     private modalCtrl: ModalController,
+    private store: Store<TripigState.State>,
   ) {
     this.initializeApp();
+  }
+
+  ngOnInit(): void {
+    this.selectedList$.subscribe(list => {
+      this.showResultRoute = list.length > 0;
+    });
   }
 
   initializeApp() {
