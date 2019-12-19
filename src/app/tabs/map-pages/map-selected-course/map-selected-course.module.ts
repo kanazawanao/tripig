fd681260235c80891990e14ed2709b53bcd3007b
kanazawanao <naomiti3333@gmail.com>
2019-12-19 16:50:57 +0900

    update

--- src/app/tabs/map-pages/map-selected-course/map-selected-course.module.ts
+++ src/app/tabs/map-pages/map-selected-course/map-selected-course.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { GoogleMapsModule } from '@angular/google-maps';
@@ -14,6 +14,9 @@ import { MapSelectedCourseComponent } from './map-selected-course.component';
     RouterModule.forChild([{ path: '', component: MapSelectedCourseComponent }]),
     GoogleMapsModule,
     MaterialModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ]
 })
 export class MapSelectedCourseModule { }
