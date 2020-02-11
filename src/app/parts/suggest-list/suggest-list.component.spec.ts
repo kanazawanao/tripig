9e06bc2de0a165467483c5bdee814dae8ff068ee
NaoKanazawa <naomiti3333@gmail.com>
2020-02-11 14:32:29 +0900

    update

--- src/app/parts/suggest-list/suggest-list.component.spec.ts
+++ src/app/parts/suggest-list/suggest-list.component.spec.ts
@@ -2,15 +2,12 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-import { provideMockStore, MockStore } from '@ngrx/store/testing';
-import * as TripigSelector from 'src/app/store/tripig.selector';
+import { provideMockStore } from '@ngrx/store/testing';
 import { SuggestListComponent } from './suggest-list.component';
-import { Store } from '@ngrx/store';
 
 describe('SuggestListComponent', () => {
   let component: SuggestListComponent;
   let fixture: ComponentFixture<SuggestListComponent>;
-  let mockStore: MockStore<any>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -29,15 +26,10 @@ describe('SuggestListComponent', () => {
       ]
     }).compileComponents();
 
-    mockStore = TestBed.inject(Store) as MockStore<any>;
     fixture = TestBed.createComponent(SuggestListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
-
-  beforeEach(() => {
-    mockStore.overrideSelector(TripigSelector.getSuggestList, []);
-  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
